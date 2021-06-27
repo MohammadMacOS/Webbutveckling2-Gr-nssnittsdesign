c2a4225672feffc25d3675f621986c6bcacc40df
MohammadMacOS <mh.haydar92@gmail.com>
2021-06-27 19:41:51 +0200

    first commit

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
-1. Ta bort filer som inte används.
-2. Skapat en ny folder för komponenter.
-3. Skpat en ny folder för att hämta bilder till webbsidan.
-4. Skpat en Way.js fil för background bild och används Tachyons för styling.
-5. Skapat Navbar.js fil för navbaren och bilder.
-6. Skpat Profile.js fil för button och input fält samt till funktioner.
-7. Skapat CSS fil för globala styling.
-8. Skapat en UseState för använda funktioner.
-9. Skapat en funktion för ta bort all resualt.
-10. Skapat en funktion för totlasumman.
-11. Använt UseEffect för använda funktionen totalsumman.
-12. Skapat en funktion för förbrukning.
-13. Skapat en Parent.js fil.
-14. Skapat en Child.js fil.
-15. Skapat en Conditionalrendering.js fil.
-16. Skapat test folder för tester.
+
+
+
+1. Skapat wireframes,  prototype för projektet
+![](images/prototype.png)
+2. Skapat ett projekt med namn 2021-06-10.
+3. starta projekt `cd 2021-06-10` `npm start`.
+4. Ta bort filer som inte används.
+5. Skapat en ny folder för komponenter.
+6. Skpat en ny folder för att hämta bilder till webbsidan.
+7. Skpat en Way.js fil för background bild och används Tachyons för styling.
+8. Skapat Navbar.js fil för navbaren och bilder.
+9. Skpat Profile.js fil för button och input fält samt till funktioner.
+10. Skapat CSS fil för globala styling.
+11. Skapat en UseState för använda funktioner.
+12. Skapat en funktion för ta bort all resualt.
+13. Skapat en funktion för totlasumman.
+14. Använt UseEffect för använda funktionen totalsumman.
+15. Skapat en funktion för förbrukning.
+16. Skapat en Parent.js fil.
+17. Skapat en Child.js fil.
+18. Skapat en Conditionalrendering.js fil.
+19. Skapat test folder för tester.
 
 
 # Webbutveckling2-Gr-nssnittsdesign
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,89 +1,107 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Profile.css";
-;
 
 export default function Profile() {
-  const [consumption, setConsumption] = useState(1);
-  const [distance, setDistance] = useState(1);
-  const [fuelCost, setFuelCost] = useState(15.73);
+  const [consumption, setConsumption] = useState('');
+  const [distance, setDistance] = useState('');
+  const [fuelCost, setFuelCost] = useState('');
   const [totalCost, setTotalCost] = useState(0);
 
-  function ClearAll() {
-    setConsumption(0);
-    setDistance(0);
-    setFuelCost(0);
-    setTotalCost(0);
-  }
 
-  function CalculateTotalCost() {
-    setTotalCost(consumption * distance * fuelCost );
-  }
 
-  useEffect(() => {
-    CalculateTotalCost();
-  }, [consumption, distance, fuelCost]);
-
-  function HandleConsumptionChange(event) {
+  const handleConsumptionChange = (event) => {
     setConsumption(event.target.value);
-  }
+  }; 
 
-  function DistanceBetweenGothenburgStockholm() {
-    setTotalCost(consumption * distance )
-  }
+  const handleDistanceChange = (event) => {
+    setDistance(event.target.value);
+};
 
-  return (
+
+const handleFuelCostChange = (event) => {
+  setFuelCost(event.target.value);
+};
+
+function ClearAll() {
+  setConsumption(0);
+  setDistance(0);
+  setFuelCost(0);
+  setTotalCost(0);
+}
+  
+ 
+let totalCostForAll  = (distance / 100 * consumption * fuelCost).toFixed(2);
+
+function NewMessage() {
+  if  (totalCostForAll  >= 1000) {
+      return 'Har du funderat på flyg?'
+  } else if (totalCostForAll  >= 400) {
+      return 'Har du funderat på att åka tåg?'
+  } else if (totalCostForAll  >= 50) {
+      return 'Resonabel resa'
+  } else {
+      return ''
+  }
+};
+
+  return ( 
     <div className="Profile-today">
-      <h3>Förbrukning(L/MIL)</h3>
-      <button className="Button-0" onClick={() => {setConsumption(4.5); setDistance(52.955); setFuelCost(366.56)}} >Göteborg till Stockholm</button>
-      <input type="number" step=".01" className="input-field" value={consumption} onChange={(event) => { HandleConsumptionChange(event); }} ></input>
+      
+      <h2 className='fl  mr30-l cl-b'>Destination till.ex Göteborg till Stockholm</h2>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <input type='text' placeholder='från'/>
+      <br/>
+      <br/>
+      <br/>
+      <input type='text' placeholder='till'/>
+
+      <h3>Förbrukning(L/KM)</h3>
+      <input type="number" step=".01" className="input-field" onChange={handleConsumptionChange} />
       <button className="Button5" onClick={() => {setConsumption(0);}}>Rensa</button>
       <br />
       <div className="grid-numer-2">
-        <h3>Sträckan(MIL)</h3>
-        <button className="Button-0" onClick={() => {setDistance(34.800000000000004);}}>Karlskrona till Göteborg</button>
-        <input
-          type="number"
-          step=".1"
-          className="input-field"
-          value={distance}
-          onChange={(event) => {
-            setDistance(event.target.value);
-          }}
-        ></input>
-        <button
-          className="Button5"
-          onClick={() => {
-            setDistance(0);
-          }}
-        >
-          Rensa
-        </button>
+        <h3>Sträckan(KM)</h3>
+        <input type="number" step=".1" className="input-field" onChange={ handleDistanceChange }/>
+        <button className="Button5" onClick={() => {setDistance(0);}}>Rensa</button>
       </div>
       <h3>Bensinpriset (KR/L)</h3>
-      <button className="Button-0" onClick={() => {setDistance(63.138000000000005);}}>Kalmar till Göteborg</button>
-      <input
-        type="number"
-        step=".01"
-        className="input-field"
-        value={fuelCost}
-        onChange={(event) => {
-          setFuelCost(event.target.value);
-        }}
-      ></input>
-      <button
-        className="Button5"
-        onClick={() => {
-          setFuelCost(0);
-        }}
-      >
-        Rensa
-      </button>
+      <input type="number" step=".01" className="input-field" onChange={ handleFuelCostChange }/>
+      <button className="Button5" onClick={() => {setFuelCost(0);}}>Rensa</button>
       <h3>TotalKostnaden</h3>
-      <button className="Button-0">TotalKostnaden</button>
-      <input type="number" className="input-field" value={totalCost}></input>
-      <button className="Button5" onClick={ClearAll}>
+      <input type="number" className="input-field" value={ totalCost }/>
+      <button className="Button5" onClick={ ClearAll }>
         Rensa allt</button>
+        <br/>
+        <br/>
+        <h2 className='fl mr-30'>TotalKostnaden <span>
+          <u>{ totalCostForAll  }</u></span></h2>
+        <br/>
+        <br/>
+        <h2><strong className='fl mr-30'>{ NewMessage() }</strong></h2>
       
     </div>
+
+
   );
-}
+
+  }
+
+
+
+  //function CalculateTotalCost() {
+    //setTotalCost(consumption * distance * fuelCost );
+  //}
+
+  //useEffect(() => {
+    //CalculateTotalCost();
+  //}, [consumption, distance, fuelCost]);
+
+  //function HandleConsumptionChange(event) {
+    //setConsumption(event.target.value);
+  //}
+
